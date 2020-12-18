12284d25e130c58177519dba3e683fd7bb3459cd
yanyi <yanyi9399@outlook.com>
2020-12-19 00:07:21 +0800

    到组件传值

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hero';
-  btnCls = 'btn btn-primary'; // 字符串，空格分隔
-  btnCls2 = ['btn', 'btn-success'];
-  btnCls3 = {
-    btn: true,
-    'btn-info': true
-  };
-  num = 1;
-  style1 = 'width: 200px;height: 50px;text-align: center;border: 1px solid;';
-  style2 = ['width', '200px', 'height', '50px', 'text-align', 'center', 'border', '1px solid']; // 有问题
-  style3 = {
-    width: '200px',
-    height: '50px',
-    'text-align': 'center',
-    border: '1px solid'
-  };
-
-
-  onClick(event: MouseEvent) {
-    console.log('onClick', event.target);
+  dialogTile = "这是AppComponent传入的title";
+  dialogShow = false;
+  
+  onDialogClosed() {
+    console.log('onDialogClosed');
+    this.dialogShow = false;
+  }
+  onDialogConfirm() {
+    console.log('onDialogConfirm');
+  }
+  showDialog() {
+    this.dialogShow = true;
   }
 }
--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-dialog',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
+  @Input() show = true;
+  @Input() title = '标题...';
+  @Input() confirmText = '确定';
+  @Input() cancelText = '取消';
+
+  @Output() closed = new EventEmitter<void>();
+  @Output() confirm = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onConfirm() {
+    this.confirm.emit();
+  }
+  onClose() {
+    this.closed.emit();
   }
 
 }
