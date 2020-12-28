ee1b40b92bbee4df973c781e222e7a7cb4c455f5
yanyi <yanyi9399@outlook.com>
2020-12-28 22:57:43 +0800

    未完待续

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,20 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  @Input('appHighlight') highlightColor: string;
 
-  constructor(el: ElementRef) { }
+  constructor(private el: ElementRef) { }
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor);
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 }
